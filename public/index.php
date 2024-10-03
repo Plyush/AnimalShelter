34efbfb9bb7e6bdf977d4096fb92f09770cfebdd
Plyush <judges.net.ru@gmail.com>
2024-10-03 07:06:09 +0300

    menu start

--- public/index.php
+++ public/index.php
@@ -4,22 +4,48 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+
+
     <link rel="stylesheet" href="css/stile.css">
     <title>Animal Shelter</title>
 </head>
 
-<h1>Первая страница PHP</h1>
 
-<?php
-echo "Это тест <br>"; // Это однострочный комментарий в стиле C++
-/* Это многострочный комментарий
-   ещё одна строка комментария */
-echo "Это ещё один тест <br>";
-echo "Последний тест <br>"; # Это комментарий в стиле оболочки Unix
-?>
+<header class="menu-bar">
+<div class="container">
+    <div class="row">
+        
+        <div id="logo" class="col-xl-2">
+        <h1 class="logo">Cozy House</h1>        
+        </div>
+
+        <div class="col-xl-5 m-lg-auto align-self-center">
+            <nav>
+        <ul class="menu d-flex justify-content-end">
+            <li class="menu__item"><a href="#">Our pets</a></li>
+            <li class="menu__item"><a href="#">About the shelter</a></li>
+            <li class="menu__item"><a href="#">To help the shelter</a></li>
+            <li class="menu__item"><a href="#">Contacts</a></li>
+        </ul>
+        </nav>
+        </div>
+    </div>
+</div>
+</header>
 
 <body>
-
+    <script async src="https://cdn.jsdelivr.net/npm/es-module-shims@1/dist/es-module-shims.min.js" crossorigin="anonymous"></script>
+    <script type="importmap">
+    {
+      "imports": {
+        "@popperjs/core": "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/esm/popper.min.js",
+        "bootstrap": "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.esm.min.js"
+      }
+    }
+    </script>
 
 
      </body>
