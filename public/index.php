7342b897b096ac841076134526d0b768ddddbeb8
Plyush <judges.net.ru@gmail.com>
2024-10-03 04:37:30 +0300

    start setup

---
+++ public/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="css/stile.css">
+    <title>Animal Shelter</title>
+</head>
+
+<h1>Первая страница PHP</h1>
+
+<?php
+echo "Это тест <br>"; // Это однострочный комментарий в стиле C++
+/* Это многострочный комментарий
+   ещё одна строка комментария */
+echo "Это ещё один тест <br>";
+echo "Последний тест <br>"; # Это комментарий в стиле оболочки Unix
+?>
+
+<body>
+
+
+
+     </body>
+    </html>
